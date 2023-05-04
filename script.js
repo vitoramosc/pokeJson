c30a5050d563d46adfcc3ac5c87508394ba4e648
Vitor Ramos <vitoramosc@gmail.com>
2023-05-04 09:06:09 -0300

    estilização dos atributos do pokemon

--- script.js
+++ script.js
@@ -1,15 +1,15 @@
 function pokemonSelecionado(pokemon){
     if(pokemon == 'pikachu'){
-        pokemonInicial.innerHTML = `<img src="src/Pokemons/Bulbassaur/bulbasaur.gif" id="pokemonSprite">`
+        pokemonInicial.innerHTML = `<img src="src/Pokemons/Pikachu/pikachu.gif" id="pokemonSprite">`
     }
     if(pokemon == 'Bulbassauro'){
         pokemonInicial.innerHTML = `<img src="src/Pokemons/Bulbassaur/bulbasaur.gif" id="pokemonSprite">`
     }
     if(pokemon == 'Charmander'){
-        pokemonInicial.innerHTML = `<img src="src/Pokemons/Bulbassaur/bulbasaur.gif" id="pokemonSprite">`
+        pokemonInicial.innerHTML = `<img src="src/Pokemons/Charmander/charmander.gif" id="pokemonSprite">`
     }
     if(pokemon == 'Squirtle'){
-        pokemonInicial.innerHTML = `<img src="src/Pokemons/Bulbassaur/bulbasaur.gif" id="pokemonSprite">`
+        pokemonInicial.innerHTML = `<img src="src/Pokemons/Squirtle/squirtle.gif" id="pokemonSprite">`
     }
     botoesAcao.innerHTML =`
     <button class="botao" id="alimentar">Alimentar</button>
@@ -17,5 +17,9 @@ function pokemonSelecionado(pokemon){
     <button class="botao" id="brincar">Brincar</button>
     <button class="botao" id="passear">Batalhar</button>
     `
+    atributos.innerHTML= `<ul>
+    <li>Nome</li>
+    </ul>`
 }
+
 
