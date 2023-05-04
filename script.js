afb43c833c169815122b0ae68be50967790e3941
henriquejm98 <henrique.mosca@sptech.school>
2023-05-04 20:39:59 -0300

    alterações no script

--- script.js
+++ script.js
@@ -1,17 +1,22 @@
-function pokemonSelecionado(pokemon){
-    if(pokemon == 'Pikachu'){
+function pokemonSelecionado(pokemon) {
+    if (pokemon == 'Pikachu') {
         pokemonInicial.innerHTML = `<img src="src/Pokemons/Pikachu/pikachu.gif" id="pokemonSprite">`
     }
-    if(pokemon == 'Bulbassauro'){
+    if (pokemon == 'Bulbassauro') {
         pokemonInicial.innerHTML = `<img src="src/Pokemons/Bulbassaur/bulbasaur.gif" id="pokemonSprite">`
     }
-    if(pokemon == 'Charmander'){
+    if (pokemon == 'Charmander') {
         pokemonInicial.innerHTML = `<img src="src/Pokemons/Charmander/charmander.gif" id="pokemonSprite">`
     }
-    if(pokemon == 'Squirtle'){
+    if (pokemon == 'Squirtle') {
         pokemonInicial.innerHTML = `<img src="src/Pokemons/Squirtle/squirtle.gif" id="pokemonSprite">`
-    }
-    atualizarStatus(pokemon)}
+
+    } 
+
+    atualizarStatus(pokemon);
+    
+    
+}
 
 
 var pokemonEscolhido = {
@@ -19,18 +24,37 @@ var pokemonEscolhido = {
     percentualVida: 100,
     percentualSujeira: 0,
     percentualFome: 0,
+    alimentar: function () {
+        if (this.percentualFome > 0 && this.percentualFome <= 100) {
+            this.percentualFome -= 10;
+        }
+    },
+    darBanho: function () {
+        if (this.percentualSujeira > 0 && this.percentualSujeira <= 100) {
+            this.percentualSujeira -= 10;
+        }
+    },
+    treinar: function () {
+        if (this.percentualVida > 0) {
+            this.percentualVida -= 30;
+            this.percentualSujeira += 30;
+        }
+    },
+    brincar: function () {
+        if(this.percentualSujeira < 100) {
+            this.percentualSujeira += 30;
+        }
+    }
 }
 
-
-
-function atualizarStatus(pokemon){
-    botoesAcao.innerHTML =`
+function atualizarStatus(pokemon) {
+    botoesAcao.innerHTML = `
     <button class="botao" id="alimentar">Alimentar</button>
     <button class="botao" id="darBanho">Dar Banho</button>
     <button class="botao" id="brincar">Brincar</button>
     <button class="botao" id="passear">Batalhar</button>
     `
-    atributos.innerHTML= `
+    atributos.innerHTML = `
     <div class="statusPokemon">
         <ul id="atributosPokemon">
             <b><li>${pokemon}</li></b>
