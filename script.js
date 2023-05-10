a08ed2ea9e8b9d5e0a3aed81549ff852cec6fd44
Vitor Ramos <vitoramosc@gmail.com>
2023-05-10 15:59:18 -0300

    função de batalha criada

--- script.js
+++ script.js
@@ -23,36 +23,47 @@ var pokemonEscolhido = {
     level: 1,
     percentualVida: 100,
     percentualSujeira: 0,
-    percentualFome: 0,
+    percentualFome: 99,
     alimentar: function () {
         if (this.percentualFome > 0 && this.percentualFome <= 100) {
             this.percentualFome -= 10;
+            document.getElementById("percentualFome").style.width = this.percentualFome +'%';
         }
     },
-    darBanho: function () {
+    banhar: function () {
         if (this.percentualSujeira > 0 && this.percentualSujeira <= 100) {
             this.percentualSujeira -= 10;
+            document.getElementById("percentualSujeira").style.width = this.percentualSujeira +'%';
         }
     },
     treinar: function () {
         if (this.percentualVida > 0) {
-            this.percentualVida -= 30;
-            this.percentualSujeira += 30;
+            this.percentualVida -= 10;
+            this.percentualSujeira += 10;
+            this.percentualFome += 10;
+            document.getElementById("percentualSujeira").style.width = this.percentualSujeira +'%';
+            document.getElementById("percentualVida").style.width = this.percentualVida +'%';
+            document.getElementById("percentualFome").style.width = this.percentualFome +'%';
+            if (this.percentualFome >=100 || this.percentualVida <=0 || this.percentualSujeira >=100){
+                alert('O pokemin morreu')
+            }
         }
     },
     brincar: function () {
         if(this.percentualSujeira < 100) {
             this.percentualSujeira += 30;
+            document.getElementById("percentualSujeira").style.width = this.percentualSujeira +'%';
+
         }
     }
 }
 
 function atualizarStatus(pokemon) {
     botoesAcao.innerHTML = `
-    <button class="botao" id="alimentar">Alimentar</button>
-    <button class="botao" id="darBanho">Dar Banho</button>
-    <button class="botao" id="brincar">Brincar</button>
-    <button class="botao" id="passear">Batalhar</button>
+    <button class="botao" id="alimentar" onclick="pokemonEscolhido.alimentar()">Alimentar</button>
+    <button class="botao" id="darBanho" onclick="pokemonEscolhido.banhar()">Dar Banho</button>
+    <button class="botao" id="brincar" onclick="pokemonEscolhido.brincar()">Brincar</button>
+    <button class="botao" id="passear" onclick="pokemonEscolhido.treinar()">Batalhar</button>
     `
     atributos.innerHTML = `
     <div class="statusPokemon">
@@ -63,9 +74,9 @@ function atualizarStatus(pokemon) {
     </div>
     <div class="atributos">
         <ul id="atributosPokemon2">
-            <div class="percentuais">Vida:<li class="barra"><span id="percentualVida"></span></li></div>
-            <div class="percentuais">Sujeira:<li class="barra"><span id="percentualSujeira"></span></li></div>
-            <div class="percentuais">Fome:<li class="barra"><span id="percentualFome"></span></li></div>
+            <div class="percentuais">Vida:<li class="barra"><span id="percentualVida" style="width: 100%;"></span></li></div>
+            <div class="percentuais">Sujeira:<li class="barra"><span id="percentualSujeira" style="width: 0%;"></span></li></div>
+            <div class="percentuais">Fome:<li class="barra"><span id="percentualFome" style="width: 99%;"></span></li></div>
         </ul>
     </div>`
 }
